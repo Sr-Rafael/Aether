57ef8d735ea46f002e342459e26a80f7382c03f0
Rafael Neves <rafaelnevesssilva@gmail.com>
2025-11-26 17:29:13 -0300

    Adição de novos módulos
    Adição de módulos de filtro por tamanho, cálculo de hash curto, filtro de hash.
    Foi deixado como comentado a próxima implementação.

--- src/includes/mod.rs
+++ src/includes/mod.rs
@@ -2,3 +2,8 @@
 // "Porteiro" que losta os módulos internos como públicos
 
 pub mod listagem;
+pub mod filtro_tamanho;
+pub mod hash_curto;
+pub mod filtro_hash_curto;
+// pub mod hash_completo;
+// pub mod filtro_hash_completo;
