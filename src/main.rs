9768803ee1a616f737145ecfc96689b8921510b2
Rafael Neves <rafaelnevesssilva@gmail.com>
2025-11-27 17:27:12 -0300

    Adicionado o uso da função para agrupamento de hash
    Foi implementado o uso das bibliotecas de calculo de hash de todo o arquivo e o agrupamento dos arquivos por hashers iguais.

--- src/main.rs
+++ src/main.rs
@@ -28,21 +28,21 @@ fn main() -> Result<(), Box<dyn Error>> {
   println!("Caminho de destino detectado: {}\n", caminho);
 
   // === Notifica o início da varredura e passa o caminho para o "responsáveç" ===
-  println!("--- Aether: Varredura de arquivos iniciada!\n");
+  println!("--- Aether: Varredura de arquivos iniciada!");
   let arquivos_encontrados = includes::listagem::list_files(caminho)?;
 
   // __ TEMPORARIO, PARA O COMPILADOR NÃO RECLAMAR __
   println!("Total de arquivos encontrados: {}\n", arquivos_encontrados.len());
 
   // === Chama a função de agrupamento ===
-  println!("\n--- Iniciando agrupamento por tamanho ---");
+  println!("--- Iniciando agrupamento por tamanho ---");
   let grupos_tamahos = includes::filtro_tamanho::agrupar_por_tamanho(arquivos_encontrados);
 
   // __ Exibe quantos agrupamentos foi realizado __
-  println!("\nAgrupamento concluído!\n");
+  println!("Agrupamento concluído!\n");
   
   // Exibe quantos grupos tem
-  println!("Total de tamanhos diferente foi: {}", grupos_tamahos.len());
+  println!("Total de grupos com tamanhos diferente foi: {}", grupos_tamahos.len());
 
   let mut potencial_duplicatas_tamanho = 0;
   for (_tamanho, lista) in &grupos_tamahos {
@@ -53,7 +53,7 @@ fn main() -> Result<(), Box<dyn Error>> {
   println!("Arquivos que contém o mesmo tamanho: {}\n", potencial_duplicatas_tamanho);
 
   // Exibe quantos arquivos com hashers iniciais parecidos tem
-  println!("\n--- Iniciando agrupamento por Hash curto 4Kb (SHA0512) ---\n");
+  println!("--- Iniciando agrupamento por Hash curto 4Kb (SHA-512) ---");
 
   // Chama a função e passa o antigo agrupamento para a função de calculo de hash curto
   let grupos_hash_curto: HashMap<String, Vec<PathBuf>> = includes::filtro_hash_curto::agrupar_por_hash(grupos_tamahos);
@@ -69,6 +69,24 @@ fn main() -> Result<(), Box<dyn Error>> {
 
   // Exibe a quantidade de arquivos com hashers parecidos
   println!("Arquivos que contém o mesmo hasher inicial: {}", potencial_duplicatas_hash);
+  
+  // Exibe os resultados dos arquivos com hasher completamente parecidos
+  println!("\n--- Iniciando agrupamento por Hash (SHA-512) ---");
+
+  // Chama a função e passa o antigo agrupamento para a função de calculo de hash curto
+  let grupos_hash: HashMap<String, Vec<PathBuf>> = includes::filtro_hash_completo::agrupar_por_hash_completo(grupos_hash_curto);
+  println!("Agrupamento concluído!\n");
+
+  // Conta os arquivos duplicados, com hashers parecidos
+  let mut duplicatas_hash = 0;
+  for (_hash, list) in &grupos_hash {
+    if list.len() > 1 {
+      duplicatas_hash += list.len();
+    }
+  }
+
+  // Exibe a quantidade de arquivos com hashers parecidos
+  println!("Arquivos que contém o mesmo hasher: {}", duplicatas_hash);
   // __ __
 
   // === Retorna sucesso em caso de der certo ===
